656bed729a34d141dfaebf09c09fca86a5124dfd
Iva Kochneva <inkybasik@gmail.com>
2020-05-17 20:18:04 +0300

    Эврика! Надо было просто убрать лишний код из .sort()

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,6 @@
 function orderByProps(object, arr) {
   const result = arr;
-  Object.entries(object).sort((a, b) => {
-    if (a[0] < b[0]) {
-      return -1;
-    }
-    if (a[0] > b[0]) {
-      return 1;
-    }
-    return 0;
-  }).forEach(([key, value]) => {
+  Object.entries(object).sort().forEach(([key, value]) => {
     const element = { key, value };
     let notFound = true;
     for (let i = 0; i < arr.length; i += 1) {
